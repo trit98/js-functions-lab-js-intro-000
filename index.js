4a65449bebd232b211d660524a8f38d58eabb865
Talia Ritholtz <talia.ritholtz@baruchmail.cuny.edu>
2018-11-20 04:43:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return "Happy Independence Day, you!"
 }
+function holidayCountdown(holiday, days) {
+  return "It\'s days until " 
+}
