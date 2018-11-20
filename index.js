83c6c4d4f65b39dd48da6c79d62473b9230bf6a7
Talia Ritholtz <talia.ritholtz@baruchmail.cuny.edu>
2018-11-20 04:38:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ function happyHolidays(string) {
 function happyHolidaysTo(name) {
   return "Happy holidays, you!"
 }
+function happyHolidayTo(holiday, name) {
+  return "Happy $(holiday), $(name)!"
+}
